fff374ee50cb51fb824ebb721867fb48de0698fa
asya-ndao <sene.asya@gmail.com>
2025-05-02 12:58:22 +0100

    feat: Create Edit button

---
+++ src/components/buttons/edit-modal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Edit = () => {
+  return (
+    <>
+      <button className='border focus:outline-none text-white bg-amber-500 hover:bg-amber-600 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer'>
+        Edit
+      </button>
+    </>
+  )
+}
+
+export default Edit
