04e99c9b3d8c7d8afe125e52632bd9700e44cbb9
asya-ndao <sene.asya@gmail.com>
2025-05-02 12:59:13 +0100

    feat: Create Delete modal button

---
+++ src/components/buttons/delete-modal.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Delete = () => {
+  return (
+    <>
+      <button
+        className='focus:outline-none text-white bg-red-600 hover:bg-red-800 focus:ring-4 
+        font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer'
+      >
+        DELETE
+      </button>
+    </>
+  )
+}
+
+export default Delete
